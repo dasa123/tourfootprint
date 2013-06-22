d3dd9bbfe70247b26df98db520754f349ef185f8
Mohamed Sharaf-El-Deen <moh.h.sharaf@gmail.com>
2013-06-22 05:24:36 +0200

    Login and Register fixed

--- app/controllers/MainPage.java
+++ app/controllers/MainPage.java
@@ -1,20 +1,29 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Email;
 import play.mvc.*;
 
 import java.io.PrintWriter;
 import java.util.*;
+
+import javax.print.attribute.standard.PagesPerMinute;
+
 import models.*;
 
 public class MainPage extends Controller {
 	
-    public static void register(String email, String password, String password_retyped) {
-        
+    public static void register(@Email String email, String password, String password_retyped) {
+        if(! validation.email(email).ok)
+        {
+        	index(false, false, 4, null);
+        	return;
+        }
+    	
     	User user = User.find("byEmail", email).first();
     	
     	if ( user == null ) {
-    		if ( password.equals(password_retyped) && password != null ) {
+    		if ( password.equals(password_retyped) && password.length() != 0) {
     			User newUser = new User(email, password, null, null, 
 						null,null,null,null,null,null);
     			newUser.save();
@@ -32,40 +41,37 @@ public class MainPage extends Controller {
     }
     
     public static void index(boolean advertisement, boolean authenticated, int error, User user) {
-    	
     	//advertisement is not shown on the main page.
-    	renderArgs.put("advertisement", false);
+    	renderArgs.put("advertisement", "false");
     	
     	if ( error ==  1 ) {
-    		renderArgs.put("passError", true);
+    		renderArgs.put("passError", "true");
     	}
     	else if ( error == 2 ){
-    		renderArgs.put("userExists", true);
+    		renderArgs.put("userExists", "true");
     	}
     	else if ( error == 3 ){
-    		renderArgs.put("loginError", true);
+    		renderArgs.put("loginError", "true");
+    	}
+    	else if ( error == 4 ){
+    		renderArgs.put("emailError", "true");
     	}
     	
     	renderArgs.put("email", user.email);
     	
     	if (authenticated == true) {
-    		renderArgs.put("authenticated", true);
+    		renderArgs.put("authenticated", "true");
     		render("pages/mainPageNotAuth.html");
     	}
     	else {
-    		renderArgs.put("authenticated", true);
+    		renderArgs.put("authenticated", "false");
     		render("pages/mainPageNotAuth.html");
     	}
     }
     
     //Login with e-mail address and password. 
     public static void login(String email, String password) {
-        System.out.println("email :" + email);
-        System.out.println("pwd:" + password);
-        
         User user = User.find("byEmailAndPassword", email, password).first();
-        
-        System.out.println("User :" + user);
         
         if(user != null) {
             session.put("userId", user.id);
