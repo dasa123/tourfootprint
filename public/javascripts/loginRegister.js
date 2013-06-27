b070d3c42695eba64786e488f28e3333b11013f5
Mohamed Sharaf-El-Deen <moh.h.sharaf@gmail.com>
2013-06-27 19:45:14 +0200

    added missing files

---
+++ public/javascripts/loginRegister.js
@@ -0,0 +1,48 @@
+var mouse_is_inside = false;
+
+$(document).ready(function() {
+    $(".login_btn").click(function() {
+        var loginBox = $("#login_box");
+        var registerBox = $("#register_box");
+        if (loginBox.is(":visible"))
+            loginBox.fadeOut("300");
+        else
+            loginBox.fadeIn("300");
+        	if (registerBox.is(":visible"))
+            	registerBox.fadeOut("300");
+        return false;
+    });
+    
+    $("#login_box").hover(function(){ 
+        mouse_is_inside=true; 
+    }, function(){ 
+        mouse_is_inside=false; 
+    });
+
+    $("body").click(function(){
+        if(! mouse_is_inside) $("#login_box").fadeOut("300");
+    });
+    
+    
+    $(".register_btn").click(function() {
+        var loginBox = $("#login_box");
+        var registerBox = $("#register_box");
+        if (registerBox.is(":visible"))
+            registerBox.fadeOut("300");
+        else
+            registerBox.fadeIn("300");
+        	if (loginBox.is(":visible"))
+        		loginBox.fadeOut("300");
+        return false;
+    });
+    
+    $("#register_box").hover(function(){ 
+        mouse_is_inside=true; 
+    }, function(){ 
+        mouse_is_inside=false; 
+    });
+
+    $("body").click(function(){
+        if(! mouse_is_inside) $("#register_box").fadeOut("300");
+    });
+});
