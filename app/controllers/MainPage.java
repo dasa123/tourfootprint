c2f8f7996e63475539bcd9b5404eea00967ba859
dasa123 <david@sardari.eu>
2013-06-30 19:23:33 +0200

    unauthenticated mainpage is dynamic

--- app/controllers/MainPage.java
+++ app/controllers/MainPage.java
@@ -62,17 +62,22 @@ public class MainPage extends Controller
 	{
 		String userId = session.get("userId");
 
+        Long mostLikedPostId = getIdOfMostLikedPost();
+        Long recentlyFinishedPostId = getIdOfRecentlyFinishedPost();
+        String mostLikedPostTitle = getTitleOfMostLikedPost(mostLikedPostId);
+        String recentlyFinishedPostTitle = getTitleOfRecentlyFinishedPost(recentlyFinishedPostId);
+
 		if (userId != null)
 		{
 			User user = User.findById(Long.parseLong(userId));
 			if (user != null)
 			{
-				render(user);
+				render(user, mostLikedPostId, mostLikedPostTitle, recentlyFinishedPostId, recentlyFinishedPostTitle);
 			}
 		}
 
 		// if no (valid) user authenticated
-		render();
+		render(mostLikedPostId, mostLikedPostTitle, recentlyFinishedPostId, recentlyFinishedPostTitle);
 	}
 
 	// Login with e-mail address and password.
@@ -127,5 +132,52 @@ public class MainPage extends Controller
 		renderArgs.put("authenticated", "false");
 
 		render();
+	}
+	
+	public static Long getIdOfRecentlyFinishedPost() {
+		
+		List<Post> posts = Post.findAll();
+		Long postId = posts.get(posts.size() - 1).id;
+
+        return postId;
+	}
+	
+	public static void getPictureOfRecentlyFinishedPost(Long postId) {
+
+		Post post = Post.findById(postId);
+		List<Image> images = post.content.pictures;
+		Random generator = new Random();
+		Long id = images.get(generator.nextInt(images.size())).getId();
+		RequestUtils.renderImage(id);
+	}
+	
+	public static String getTitleOfRecentlyFinishedPost(Long postId) {
+		
+		Post post = Post.findById(postId);
+		return post.title;
+	}
+	
+	public static Long getIdOfMostLikedPost() {
+		
+		List<Post> posts = Post.findAll();
+		Random generator = new Random();
+		Long postId = posts.get(generator.nextInt(posts.size() - 1)).id;
+
+        return postId;
+	}
+	
+	public static void getPictureOfMostLikedPost(Long postId) {
+
+		Post post = Post.findById(postId);
+		List<Image> images = post.content.pictures;
+		Random generator = new Random();
+		Long id = images.get(generator.nextInt(images.size())).getId();
+		RequestUtils.renderImage(id);
+	}
+	
+	public static String getTitleOfMostLikedPost(Long postId) {
+		
+		Post post = Post.findById(postId);
+		return post.title;
 	}
 }
