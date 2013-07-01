b97dbbd7249e178421254ac3c0de0f185eaa0797
Mohamed Sharaf-El-Deen <moh.h.sharaf@gmail.com>
2013-07-01 11:15:22 +0200

    Models: added getMostRecent and getMostLiked to Post

--- app/models/Post.java
+++ app/models/Post.java
@@ -53,4 +53,64 @@ public class Post extends Model
 		
 		this.rating = 0.0;
 	}
+	
+	public static Post getMostRecent()
+	{
+		List<Post> posts = Post.findAll();
+		
+		if(posts.size() > 0)
+		{
+			return posts.get(posts.size() - 1);
+		}
+		
+		return null;
+	}
+	
+	public static Post getMostRecentWithImages()
+	{
+		List<Post> posts = Post.findAll();
+		
+		for (int i = posts.size() - 1; i >= 0; i--) {
+			Post post = posts.get(i);
+			if (post.content.pictures.size() > 0) {
+				return post;
+			}
+		}
+
+		return null;
+	}
+	
+
+	public static Post getfMostLikedWithImages() {
+		
+		List<Post> chronologicalPosts = Post.findAll();
+		
+		if (chronologicalPosts.size() > 1) {
+
+			List<Post> posts = Post.find("order by rating desc").fetch();
+			Long mostRecentPostId = chronologicalPosts.get(chronologicalPosts.size() - 1).getId();
+
+			for (int i = 0; i < posts.size() - 1; i++) {
+				if (posts.get(i).content.pictures.size() > 0 && posts.get(i).getId().compareTo(mostRecentPostId) != 0) {
+					return posts.get(i);
+				}
+			}
+		}
+
+		return null;
+	}
+	
+	public static Post getfMostLiked() {
+		
+		List<Post> chronologicalPosts = Post.findAll();
+		
+		if (chronologicalPosts.size() > 1) {
+
+			List<Post> posts = Post.find("order by rating desc").fetch();
+
+			return posts.get(0);
+		}
+
+		return null;
+	}
 }
