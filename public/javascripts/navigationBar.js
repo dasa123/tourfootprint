55c70b5d7be1b03955a78999328a3d0fedd31c38
Mohamed Sharaf-El-Deen <moh.h.sharaf@gmail.com>
2013-07-06 23:23:16 +0200

    IDs of template and navigation made conflict free

--- public/javascripts/navigationBar.js
+++ public/javascripts/navigationBar.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 	/*
 	 * Login Popup
 	 */
-    $(".login_button").click(function() {
-        var loginBox = $("#login_box");
-        var registerBox = $("#register_box");
+    $(".navigation_bar_login_button").click(function() {
+        var loginBox = $("#navigation_bar_login_box");
+        var registerBox = $("#navigation_bar_register_box");
         if (loginBox.is(":visible"))
             loginBox.fadeOut("300");
         else
@@ -17,22 +17,22 @@ $(document).ready(function() {
         return false;
     });
     
-    $("#login_box").hover(function(){ 
+    $("#navigation_bar_login_box").hover(function(){ 
         mouse_is_inside=true; 
     }, function(){ 
         mouse_is_inside=false; 
     });
 
     $("body").click(function(){
-        if(! mouse_is_inside) $("#login_box").fadeOut("300");
+        if(! mouse_is_inside) $("#navigation_bar_login_box").fadeOut("300");
     });
     
 	/*
 	 * Register Popup
 	 */
-    $(".register_button").click(function() {
-        var loginBox = $("#login_box");
-        var registerBox = $("#register_box");
+    $(".navigation_bar_register_button").click(function() {
+        var loginBox = $("#navigation_bar_login_box");
+        var registerBox = $("#navigation_bar_register_box");
         if (registerBox.is(":visible"))
             registerBox.fadeOut("300");
         else
@@ -42,21 +42,21 @@ $(document).ready(function() {
         return false;
     });
     
-    $("#register_box").hover(function(){ 
+    $("#navigation_bar_register_box").hover(function(){ 
         mouse_is_inside=true; 
     }, function(){ 
         mouse_is_inside=false; 
     });
 
     $("body").click(function(){
-        if(! mouse_is_inside) $("#register_box").fadeOut("300");
+        if(! mouse_is_inside) $("#navigation_bar_register_box").fadeOut("300");
     });
     
 	/*
 	 * Profile Popup
 	 */
-    $(".profile_button").click(function() {
-        var profileBox = $("#profile_box");
+    $(".navigation_bar_profile_button").click(function() {
+        var profileBox = $("#navigation_bar_profile_box");
         
         if (profileBox.is(":visible"))
             profileBox.fadeOut("300");
@@ -65,13 +65,13 @@ $(document).ready(function() {
         return false;
     });
     
-    $("#profile_box").hover(function(){ 
+    $("#navigation_bar_profile_box").hover(function(){ 
         mouse_is_inside=true; 
     }, function(){ 
         mouse_is_inside=false; 
     });
 
     $("body").click(function(){
-        if(! mouse_is_inside) $("#profile_box").fadeOut("300");
+        if(! mouse_is_inside) $("#navigation_bar_profile_box").fadeOut("300");
     });
 });
